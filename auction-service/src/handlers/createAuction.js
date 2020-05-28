c586b4df02d6698058d241ae67005d2c4b02f7e6
Edvin Dzidic <edvindzidic2000@gmail.com>
2020-05-28 19:51:48 +0200

    Add iam, resources folder, getAuction, getAuctions and placeBid handlers and a lib folder with commonMiddleware

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,10 +1,12 @@
 import { v4 as uuid } from "uuid";
 import AWS from "aws-sdk";
+import commonMiddleware from "../lib/commonMiddleware";
+import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
 
   const now = new Date();
 
@@ -13,14 +15,22 @@ async function createAuction(event, context) {
     title,
     status: "OPEN",
     createdAt: now.toISOString(),
+    highestBid: {
+      amount: 0,
+    },
   };
 
-  await dynamodb
-    .put({
-      TableName: "AuctionsTable",
-      Item: auction,
-    })
-    .promise();
+  try {
+    await dynamodb
+      .put({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction,
+      })
+      .promise();
+  } catch (err) {
+    console.log(err);
+    throw new createError.InternalServerError(err);
+  }
 
   return {
     statusCode: 201,
@@ -28,4 +38,4 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+export const handler = commonMiddleware(createAuction);
