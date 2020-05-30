2d610e8b9a2a86af1cabc4da99bd1b87cf6e6619
Edvin Dzidic <edvindzidic2000@gmail.com>
2020-05-30 13:11:34 +0200

    Add notification-service, configured auth-service

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -10,11 +10,20 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
 
   if (auction.status !== "OPEN") {
     throw new createError.Forbidden("You cannot bid on closed auctions!");
+  }
+
+  if (email === auction.seller) {
+    throw new createError.Forbidden("You cannot bid on your own auctions!");
+  }
+
+  if (email === auction.highestBid.bidder) {
+    throw new createError(`You are already the highest bidder!`);
   }
 
   if (amount <= auction.highestBid.amount) {
@@ -26,9 +35,11 @@ async function placeBid(event, context) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: "set highestBid.amount = :amount",
+    UpdateExpression:
+      "set highestBid.amount = :amount, highestBid.bidder = :bidder",
     ExpressionAttributeValues: {
       ":amount": amount,
+      ":bidder": email,
     },
     ReturnValues: "ALL_NEW",
   };
